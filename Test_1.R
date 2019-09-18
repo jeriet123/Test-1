560348457063ceef52abb568a7040b6624c86694
jeriet123 <jerietr@gmail.com>
2019-09-18 15:01:49 -0500

    Operators

---
+++ Test_1.R
@@ -0,0 +1,29 @@
+a <- 5:15
+b <- a[1]
+c <- a[7]
+b < c
+b > c
+b >= c
+x <- a[2]
+y <- a[6]
+z <- a[9]
+((z+x)*(z+y))/2
+10*(x-y)
+# != is the operator for negation
+c%%b
+# %% is dividing b from c and retuning the remainder
+c^b
+# ^ raises c to the power of b
+c%/%b
+# %/% is dividing b from c and returning the answer in whole number form with out remainder
+Operator <- function(x){x^3-20}
+Operator(3)
+23 <- yah
+0/0
+# NAN means not a number and occurs when the computer knows the math you are doing is impossible
+400/0
+-400/0
+# -INF and INF means infinity and occurs when math is don that the computer cant comprehend
+matrix()
+# NA means not apllicable and occurs when a value is missing
+
